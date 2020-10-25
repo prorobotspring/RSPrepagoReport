549e9fa4e9e9b3ab89e769cec5e5dbde6fafde65
prorobotspring <prorobotspring@gmail.com>
2020-10-25 00:14:47 -0400

    version 1

---
+++ runReport.sh
@@ -0,0 +1,1 @@
+/servintdev/jdk1.6.0_25/bin/java -classpath .:lib/bssintutils.jar:lib/jdom.jar:lib/log4j-1.2.15.jar:/servintdev/wls10.3.0/wlserver_10.3/server/lib/weblogic.jar -jar /appsintdev/digitel/applications/RSPrepagoReport/RSPrepagoReport.jar
